2f44479f6518289e6d7e3d43b4990c6e29a97f21
SANJIB KUMAR DAS <sanjibdas.23012002@gmail.com>
2023-03-26 11:11:57 +0530

    operators

--- operators.py
+++ operators.py
@@ -1,7 +1,30 @@
 a = 10
 b = 3
 
-print("num1 + num2 is: ", a+b)
-print("num1 - num2 is: ", a-b)
-print("num1 * num2 is: ", a*b)
-print("num1 / num2 is: ", a/b)
+# Arithmetic Operators
+print("num1 + num2 is: ", a+b)  #addition
+print("num1 - num2 is: ", a-b)  #subtraction
+print("num1 * num2 is: ", a*b)  #multiplication
+print("num1 / num2 is: ", a/b)  #division
+
+print("num1 ** num2 is: ", a**b)  #a^b
+print("num1 // num2 is: ", a//b)  #floor(a/b)
+print("num1 % num2 is: ", a%b)  #(a mod b)
+
+# Assignment Operators
+a += 3
+print(a)
+
+a -= 5
+print(a)
+
+a *= 3
+print(a)
+
+a /= 4
+print(a)
+
+# Comparison Operators
+a = 10
+b = 5
+print(a < b)
